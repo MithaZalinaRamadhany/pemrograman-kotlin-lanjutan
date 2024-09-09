7478ff05ab10215c8ea604668b87c3a5d4332a7a
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-09 19:16:58 +0700

    add Array.kt

---
+++ src/main/kotlin/Set.kt
@@ -0,0 +1,15 @@
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+fun main() {
+    //Set<T> stores unique elements; their order is generally undefined.
+    val numbers = setOf(1, 2, 3, 4)
+    println("Number of Elements: ${numbers.size}")
+    if (numbers.contains(1)) println("1 is in the set")
+
+    val numbersBackwards = setOf(4, 3, 2, 1)
+    println("The sets are equal: ${numbers == numbersBackwards}")
+
+    //The default implementation of Set – LinkedHashSet – preserves the order of elements insertion
+    println(numbers.first() == numbersBackwards.first())
+
+}
