66f1f5fc6f027fe2f6252544e4358299ac2bd8c4
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-14 00:12:38 +0700

    push stater project.

---
+++ src/main/kotlin/NullDiperbolehkan.kt
@@ -0,0 +1,8 @@
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+fun main(args: Array<String>) {
+    // tipe label null
+    var mayNumber : Int? = 15
+    mayNumber = null
+    println("MayNumber: $mayNumber ${mayNumber?.hashCode()}")
+}
