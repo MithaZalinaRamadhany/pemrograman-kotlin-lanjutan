eddd122570533d3ef54726c525b7d53e423013bb
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-14 00:35:53 +0700

    push stater project.

---
+++ src/main/kotlin/NullEvilsOperator.kt
@@ -0,0 +1,7 @@
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+fun main(args: Array<String>) {
+    //evils operator ?:
+    var maybeWelcome: String? = "Hello World"
+    println(maybeWelcome?.length ?: 0)
+}
