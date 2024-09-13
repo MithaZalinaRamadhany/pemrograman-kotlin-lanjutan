6d0ddcc70bdb14f02cdb801a6c1ad10bd6cb187c
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-14 00:30:37 +0700

    push stater project.

---
+++ src/main/kotlin/NullPemeriksaanKondisi.kt
@@ -0,0 +1,10 @@
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+fun main(args: Array<String>) {
+    //kondisi
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The entered value isn't a number")
+}
---
+++ src/main/kotlin/NullSafeCalls.kt
@@ -0,0 +1,11 @@
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+fun main(args: Array<String>) {
+    //pemanggilan yang aman
+    //?.Let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it)}
+
+    // the ?. chaining
+    //student?.teacher?.supervisor?.name = "Seymour Skinner"
+}
